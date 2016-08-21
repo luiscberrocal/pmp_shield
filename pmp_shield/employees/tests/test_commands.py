0bc1ba0846cb634e8ec07b2f66dbd5f69c9a7a7c
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2016-08-21 09:08:57 -0500

    Added mocks for ldap to employee command tests

--- pmp_shield/employees/tests/test_commands.py
+++ pmp_shield/employees/tests/test_commands.py
@@ -1,4 +1,5 @@
 import base64
+from unittest import mock
 
 from django.test import TestCase
 
@@ -15,23 +16,46 @@ from ..models import Employee
 
 class TestCreateEmployeeCommand(TestCase):
 
-
     def test_create_employee(self):
-        content = StringIO()
-        call_command('create_employee', usernames=['lberrocal', 'vjaen'], stdout=content)
-        results = self.get_results(content)
+        def my_search_by_username(myself, username):
+            ldap_data_lberrocal = [{'company_id': '1865325',
+                                   'first_name': 'Luis',
+                                   'last_name': 'Berrocal',
+                                   'username': 'lberrocal',
+                                   'email': 'lberrocal@pancanal.com',
+                                   'office': 'TINO-NS',
+                                   'phone': '272-4149'}]
+            ldap_data_vjaen = [{'company_id': '1234567',
+                               'first_name': 'Víctor',
+                               'last_name': 'Jaen',
+                               'username': 'vjaen',
+                               'email': 'vjaen@pancanal.com',
+                               'office': 'TINO-NS',
+                               'phone': '272-4148'}]
+            if username == 'lberrocal':
+                return ldap_data_lberrocal
+            else:
+                return ldap_data_vjaen
+        with mock.patch('pmp_shield.employees.ldap_tools.LDAPTool.search_by_username', my_search_by_username):
+            content = StringIO()
+            call_command('create_employee', usernames=['lberrocal', 'vjaen'], stdout=content)
+            results = self.get_results(content)
 
-        self.assertEqual(2, Employee.objects.count())
-        self.assertEqual(2, len(results))
+            self.assertEqual(2, Employee.objects.count())
+            self.assertEqual('Berrocal', Employee.objects.get(username='lberrocal').last_name)
+            self.assertEqual('Jaen', Employee.objects.get(username='vjaen').last_name)
+            self.assertEqual(2, len(results))
 
     def test_create_employee_doesnot_exist_in_ldap(self):
-        content = StringIO()
-        call_command('create_employee', usernames=['dad'], stdout=content)
-        results = self.get_results(content)
+        def my_search_by_username(myself, username):
+            return []
+        with mock.patch('pmp_shield.employees.ldap_tools.LDAPTool.search_by_username', my_search_by_username):
+            content = StringIO()
+            call_command('create_employee', usernames=['dad'], stdout=content)
+            results = self.get_results(content)
 
-        self.assertEqual(0, Employee.objects.count())
-        self.assertEqual('Could not create employee for username dad', results[0])
-
+            self.assertEqual(0, Employee.objects.count())
+            self.assertEqual('Could not create employee for username dad', results[0])
 
     def get_results(self, content):
         content.seek(0)
