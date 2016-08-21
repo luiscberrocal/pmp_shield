4a85834c3a36bb78c62102c1c77f3d54a521cfdf
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2016-08-21 12:20:48 -0500

    Added employees list filter for unit

--- pmp_shield/employees/urls.py
+++ pmp_shield/employees/urls.py
@@ -4,4 +4,5 @@ from .views import EmployeeListView
 
 urlpatterns = [
     url(r'^$', EmployeeListView.as_view(), name='employee-list'),
+    url(r'^assigned/(?P<office_slug>[\w-]*)', EmployeeListView.as_view(), name='employee-list-assigned'),
 ]
--- pmp_shield/employees/views.py
+++ pmp_shield/employees/views.py
@@ -4,9 +4,26 @@ from django.shortcuts import render
 # Create your views here.
 from django.views.generic import ListView
 
-from .models import Employee
+from .models import Employee, OrganizationUnit
 
 
 class EmployeeListView(LoginRequiredMixin, ListView):
     model = Employee
+
+    def get_context_data(self, **kwargs):
+        context = super(EmployeeListView, self).get_context_data(**kwargs)
+        if self.kwargs.get('office_slug'):
+            #office = OrganizationUnit.objects.get(slug=self.kwargs.get('office_slug'))
+            context['title'] = 'Employees %s' % str(self.office)
+        else:
+            context['title'] = 'Employees'
+        return context
+
+    def get_queryset(self):
+        qs = super(EmployeeListView, self).get_queryset()
+        if self.kwargs.get('office_slug'):
+            self.office = OrganizationUnit.objects.get(slug=self.kwargs.get('office_slug'))
+            return Employee.objects.currently_assigned_to(self.office)
+        else:
+            return qs
 
