78535639b5d84e361710d872ccc0fc1ebebe72d5
Luis Carlos Berrocal <luis.berrocal.1942@gmail.com>
2016-08-31 13:46:27 -0500

    Create goals command with tests

--- pmp_shield/goals/management/commands/create_goals_for_projects.py
+++ pmp_shield/goals/management/commands/create_goals_for_projects.py
@@ -4,8 +4,8 @@ from django.core.management import BaseCommand
 from django.conf import settings
 import logging
 
-from ....projects.models import Project
-from ...models import Employee, Goal
+from ....projects.models import Project, ProjectMembership
+from ...models import Employee, Goal, EmployeeGoal, ExpectationsList
 
 logger = logging.getLogger(__name__)
 
@@ -22,9 +22,14 @@ class Command(BaseCommand):
                             help="Office short name",
                             )
 
+        parser.add_argument("--fiscal-year",
+                            dest="fiscal_year",
+                            help="Fical Year",
+                            )
+
 
     def handle(self, *args, **options):
-        projects = Project.objects.filter(office__short_name=options['office'])
+        projects = Project.objects.filter(office__short_name=options['office'], fiscal_year=options['fiscal_year'])
         count = 0
         count_created = 0
         for project in projects:
@@ -33,6 +38,16 @@ class Command(BaseCommand):
                                     % (90,  end_date.strftime('%d-%b-%Y'))
             goal, created = Goal.objects.get_or_create(project=project, expectations=expectations)
             self.stdout.write('Goal: %s created: %s' % (goal, created))
+            for member in project.members.all():
+                employee_expectations = expectations
+                if member.role == ProjectMembership.LEADER_ROLE:
+                    employee_expectations +=' Apoyar al supervisor en la gestión administrativa.'
+                expectations_list, _ = ExpectationsList.objects.get_or_create(version=1, employee=member.member,
+                                                                           fiscal_year='AF16')
+
+                employee_goal, _ = EmployeeGoal.objects.get_or_create(employee=member.member, goal=goal,
+                                                            expectations=employee_expectations, weight= 0.1,
+                                                            expectations_list=expectations_list)
             count += 1
             if created:
                 count_created += 1
